fcfae7251b05e45196d70958f4916c39ae8fdd2c
KH2Wone <kkhw1202@gmail.com>
2023-09-20 01:14:59 +0900

    Add: 메인 페이지 화면 기초 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,30 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="min-w-screen min-h-screen bg-black text-white">
+      <header>
+        <h1 className="text-center py-9 px-1 text-4xl">🤖 Calc App 🤖</h1>
+      </header>
+      <main className="w-10/12 my-8 mx-auto">
+        <ul className="flex flex-auto gap-x-4 gap-y-5 flex-wrap">
+          <li className="rounded-md border border-zinc-600 flex-initial text-center w-[calc(50%-0.5rem)] hover:bg-zinc-600 active:bg-zinc-600 transition-all">
+            <Link href="/bmi" className="w-full block px-5 py-5">
+              BMI Calculator
+            </Link>
+          </li>
+          <li className="rounded-md border border-zinc-600 flex-initial text-center w-[calc(50%-0.5rem)] hover:bg-zinc-600 active:bg-zinc-600 transition-all">
+            <Link href="/tip" className="w-full block px-5 py-5">
+              Tip Calculator
+            </Link>
+          </li>
+          <li className="rounded-md border border-zinc-600 flex-initial text-center w-[calc(50%-0.5rem)] hover:bg-zinc-600 active:bg-zinc-600 transition-all">
+            <Link href="/tip" className="w-full block px-5 py-5">
+              Preparing
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
